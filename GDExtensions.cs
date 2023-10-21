6c73e3c3e9135e76572abf3af5adf89aff4f8198
Ilnaz Khasanov <ilnaz.khasanoff2003@gmail.com>
2023-10-21 15:58:51 +0300

    Добавьте файлы проекта.

---
+++ GDExtensions.cs
@@ -0,0 +1,58 @@
+﻿using Godot;
+
+namespace SkiaSharp.Views.Godot;
+
+public static class GDExtensions
+{
+	// Point
+	public static SKPoint ToSKPoint(this Vector2 point) => new(point.X, point.Y);
+	public static Vector2 ToGDPoint(this SKPoint point) => new(point.X, point.Y);
+
+	// Rect
+	public static SKRect ToSKRect(this Rect2 rect) => new(rect.Position.X, rect.Position.Y, rect.End.X, rect.End.Y);
+	public static Rect2 ToGDRect(this SKRect rect) => new(rect.Left, rect.Top, rect.Width, rect.Height);
+
+	// Color
+	public static SKColor ToSKColor(this Color color) => new(color.ToRgba32());
+	public static Color ToGDColor(this SKColor color) => new((uint)color);
+
+
+    // Image
+    public static Image ToGDImage(this SKPicture picture, SKSizeI dimensions)
+    {
+        using var skiaImage = SKImage.FromPicture(picture, dimensions);
+        return skiaImage.ToGDImage();
+    }
+
+    public static Image ToGDImage(this SKImage skiaImage)
+    {
+        var gdImage = Image.Create(skiaImage.Info.Width, skiaImage.Info.Height, false, Image.Format.Rgba8);
+        gdImage.LoadPngFromBuffer(skiaImage.Encode().ToArray());
+        return gdImage;
+    }
+
+    public static Image ToGDImage(this SKBitmap skiaBitmap)
+    {
+        using var pixmap = skiaBitmap.PeekPixels();
+        using var skiaImage = SKImage.FromPixels(pixmap);
+        return skiaImage.ToGDImage();
+    }
+
+    public static Image ToGDImage(this SKPixmap pixmap)
+    {
+        using var skiaImage = SKImage.FromPixels(pixmap);
+        return skiaImage.ToGDImage();
+    }
+
+    //public static SKBitmap ToSKBitmap(this Image gdImage)
+    //{
+    //}
+
+    //public static SKImage ToSKImage(this Image gdImage)
+    //{
+    //}
+
+    //public static void ToSKPixmap(this Image gdImage, SKPixmap pixmap)
+    //{
+    //}
+}
---
+++ SKControl.cs
@@ -0,0 +1,42 @@
+﻿using Godot;
+
+using SkiaSharp.Views.Desktop;
+
+namespace SkiaSharp.Views.Godot;
+
+public partial class SKControl : Control
+{
+	public event EventHandler<SKPaintSurfaceEventArgs>? PaintSurface;
+
+    private ImageTexture? _imageTexture;
+
+    public override void _Draw()
+    {
+        int width = (int)Size.X,
+            height = (int)Size.Y;
+
+        if (Visible == false || width == 0 || height == 0)
+            return;
+
+        var imageInfo = new SKImageInfo(width, height, SKColorType.Rgba8888, SKAlphaType.Premul);
+        using var surface = SKSurface.Create(imageInfo);
+        
+        OnPaintSurface(new SKPaintSurfaceEventArgs(surface, imageInfo));
+
+        surface.Canvas.Flush();
+            
+        var image = Image.Create(width, height, false, Image.Format.Rgba8);
+        image.LoadPngFromBuffer(surface.Snapshot().Encode().ToArray());
+
+        if (_imageTexture is null)
+            _imageTexture = ImageTexture.CreateFromImage(image);
+        else if (_imageTexture.GetSize() != image.GetSize())
+            _imageTexture.SetImage(image);
+        else
+            _imageTexture.Update(image);
+
+        DrawTextureRect(_imageTexture, new Rect2(0, 0, width, height), false);
+	}
+
+	protected virtual void OnPaintSurface(SKPaintSurfaceEventArgs e) => PaintSurface?.Invoke(this, e);
+}
