cf843f50dd89cc76520fd353fc3d4e1bbde45dfb
Ilnaz <ilnaz.hasanoff2003@gmail.com>
2023-10-23 17:16:02 +0300

    Draw logic speeded up.

--- SKControl.cs
+++ SKControl.cs
@@ -1,6 +1,7 @@
 ﻿using Godot;
 
 using SkiaSharp.Views.Desktop;
+using System.Runtime.InteropServices;
 
 namespace SkiaSharp.Views.Godot;
 
@@ -8,11 +9,11 @@ public partial class SKControl : Control
 {
 	public event EventHandler<SKPaintSurfaceEventArgs>? PaintSurface;
 
-    private Image? _image;
-    private ImageTexture? _imageTexture;
-
     private SKImageInfo? _imageInfo;
+    private byte[]? _imageData;
+    private GCHandle? _imageDataHandle;
     private SKSurface? _surface;
+    private ImageTexture? _imageTexture;
 
     public override void _Draw()
     {
@@ -24,38 +25,37 @@ public partial class SKControl : Control
 
         if (_imageInfo is null || _imageInfo.Value.Width != width || _imageInfo.Value.Height != height)
         {
-            _imageInfo = new SKImageInfo(width, height, SKColorType.Rgba8888, SKAlphaType.Premul);
-            
+            _imageInfo = new SKImageInfo(width, height, SKColorType.Rgba8888, SKAlphaType.Unpremul);
+
             _surface?.Dispose();
-            _surface = SKSurface.Create(_imageInfo.Value);
+            _imageDataHandle?.Free();
+
+            _imageData = new byte[_imageInfo.Value.BytesSize];
+            _imageDataHandle = GCHandle.Alloc(_imageData, GCHandleType.Pinned);
+            _surface = SKSurface.Create(_imageInfo.Value, _imageDataHandle.Value.AddrOfPinnedObject(), _imageInfo.Value.RowBytes);
         }
 
-        _surface!.Canvas.Clear();
-
+        //_surface!.Canvas.Clear();
         OnPaintSurface(new SKPaintSurfaceEventArgs(_surface, _imageInfo.Value));
+        _surface!.Canvas.Flush();
 
-        _surface.Canvas.Flush();
-        
-        if (_image is null || _image.GetWidth() != width || _image.GetHeight() != height)
-            _image = Image.Create(width, height, false, Image.Format.Rgba8);
-
-        _image.LoadPngFromBuffer(_surface.Snapshot().Encode().ToArray());
+        var image = Image.CreateFromData(width, height, false, Image.Format.Rgba8, _imageData);
 
         if (_imageTexture is null)
-            _imageTexture = ImageTexture.CreateFromImage(_image);
-        else if (_imageTexture.GetSize() != _image.GetSize())
-            _imageTexture.SetImage(_image);
+            _imageTexture = ImageTexture.CreateFromImage(image);
+        else if (_imageTexture.GetSize() != image.GetSize())
+            _imageTexture.SetImage(image);
         else
-            _imageTexture.Update(_image);
+            _imageTexture.Update(image);
 
         DrawTextureRect(_imageTexture, new Rect2(0, 0, width, height), false);
-	}
+    }
 
     public override void _ExitTree()
     {
-        _image?.Dispose();
-        _imageTexture?.Dispose();
+        _imageDataHandle?.Free();
         _surface?.Dispose();
+        _imageTexture?.Dispose();
     }
 
     protected virtual void OnPaintSurface(SKPaintSurfaceEventArgs e) => PaintSurface?.Invoke(this, e);
